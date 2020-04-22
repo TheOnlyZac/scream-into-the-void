aaebc32c7c4d1a24e3ef1e000d52b1aa636840ad
Zac <ZacharyKrasnow@gmail.com>
2020-04-22 04:32:54 -0400

    improved documentation

--- void.js
+++ void.js
@@ -15,10 +15,10 @@ $(document).ready(function() {
         if (keycode === 8) voidText.removeChild(voidText.lastChild);
         else if (keycode === 13) voidText.appendChild(document.createTextNode('\n'));
         
-        // can't print
+        // can't print, invalid character or currently animating
         if (e.key.length != 1  || animating) return;
 
-        // create container for new letter
+        // create span container for new letter
         let span = document.createElement('span'),
         char = e.key;
 
@@ -32,11 +32,12 @@ $(document).ready(function() {
         voidText.appendChild(span);
     })
 
+    // every 100ms, decrease the timer by 100ms and check if 0
     setInterval(function() {
         countdown -= 100;
         if (countdown < 0) countdown = 0;
-        console.log(countdown);
 
+        // if the counter is 0 and not already animating, fadeout the void text
         if (countdown === 0 && voidText.children.length > 0 && !animating) {
             countdown = 0;
             fadeVoidText();
@@ -45,18 +46,25 @@ $(document).ready(function() {
 })
 
 function fadeVoidText() {
+    // fade out the voidtext 1 char at a time
     var spans = Array.from(document.getElementById('void').children),
     fadeTime;
 
+    // set fadeout time to num of chars * 100ms, with a max of 2000ms (2s)
     if (spans.length <= 20) time = spans.length * 100;
     else time = 2000;
 
+    // set animating to true to disallow user input
+    animating = true;
+
+    // iterate over each character
     for (let i = 0; i < spans.length; i++) {
-        animating = true;
+        // fadeout each character 100ms apart
         setTimeout(function() {
             $(spans[i]).animate({opacity: 0}, 1000);
         }, i * (time/spans.length));
 
+        // if this is the last character, clear the void text and set animation false
         if (i === spans.length-1) {
             setTimeout(function() {
                 $('#void').empty();
